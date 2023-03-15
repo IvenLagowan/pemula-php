1caa85612d6f25f7e4e26c3d084b1d37fb5a2b9c
Your Name <you@example.com>
2023-03-15 06:45:12 -0700

    update dari files hasil belajar books

---
+++ variables.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menggenal Variables</title>
+</head>
+<body>
+    
+    <!-- Menggenal Variables -->
+    <?php
+
+    $nama = "Dikamu";
+    
+    $anda = "Neko";
+    printf("Hello, %s", $nama);
+    
+    printf("Neko, %s", $anda);
+
+    
+
+    ?>
+</body>
+</html>
